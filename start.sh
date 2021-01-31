fa441d1d719506db0974a16c91a775929f6a84bb
stratnevpy <78310001+stratnevpy@users.noreply.github.com>
2021-01-31 20:14:21 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-#VPN=uriit
 VPN=$1
 PORT=$2
 NET=$3
 MASK=$4
-#WORKDIR=/opt/ovpn
 DOCKER=~/docker
 
-#rm -rf $WORKDIR/$VPN
-#mkdir $WORKDIR/$VPN
 cd $DOCKER
 docker build -t ovpn .
 docker run -d --name $VPN --restart unless-stopped --cap-add=NET_ADMIN -v /opt/ovpn/$VPN:/opt/ovpn -v /etc/localtime:/etc/localtime:ro -p $PORT:1194/udp ovpn $VPN $PORT $NET $MASK
