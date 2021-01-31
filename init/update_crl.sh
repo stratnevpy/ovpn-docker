00da03ff9a07eaa6172d68d1e2505d2bc92d796f
stratnevpy <78310001+stratnevpy@users.noreply.github.com>
2021-01-31 20:19:04 +0500

    Update update_crl.sh

--- init/update_crl.sh
+++ init/update_crl.sh
@@ -5,5 +5,4 @@ EASYRSADIR=$BASEDIR/easy-rsa
 
 $EASYRSADIR/easyrsa gen-crl
 chmod 644 $BASEDIR/pki/crl.pem
-#cp $BASEDIR/pki/crl.pem $VPNDIR/crl.pem
 if [ ! -L $VPNDIR/crl.pem ]; then ln -s $BASEDIR/pki/crl.pem $VPNDIR/crl.pem; fi
